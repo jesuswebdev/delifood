376e048f555623b2736594bde6f49c7b483f822e
Jesús Jiménez <jesuswebdev@gmail.com>
2021-12-03 16:01:20 -0400

    add function

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,2 +1,13 @@
+import { Types } from 'mongoose';
+
+export const castToObjectId = function castToObjectId(
+  value: string | Types.ObjectId
+) {
+  if (typeof value === 'string') {
+    return new Types.ObjectId(value);
+  }
+  return value;
+};
+
 export * from './interfaces/index';
 export * from './schemas/index';
