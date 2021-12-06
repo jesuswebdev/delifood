529c7afd7dac48dff7f7edadd9d37583a431af03
Jesús Jiménez <jesuswebdev@gmail.com>
2021-12-06 15:47:00 -0400

    add LeanPermissionDocument to permission interfaces

--- common/src/interfaces/permission.ts
+++ common/src/interfaces/permission.ts
@@ -1,4 +1,4 @@
-import { Document, Model } from 'mongoose';
+import { Document, Model, LeanDocument } from 'mongoose';
 
 export interface PermissionAttributes {
   name: string;
@@ -8,4 +8,9 @@ export interface PermissionAttributes {
 
 export interface PermissionDocument extends Document, PermissionAttributes {}
 
+// eslint-disable-next-line
+export interface LeanPermissionDocument
+  extends LeanDocument<PermissionAttributes> {}
+
+// eslint-disable-next-line
 export interface PermissionModel extends Model<PermissionDocument> {}
