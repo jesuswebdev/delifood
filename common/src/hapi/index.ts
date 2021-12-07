93ddacb4442b20a01b27e8bec4edf9a9b968e32b
Jesús Jiménez <jesuswebdev@gmail.com>
2021-12-07 16:48:20 -0400

    add hapi plugin interfaces

--- common/src/hapi/index.ts
+++ common/src/hapi/index.ts
@@ -1,1 +1,10 @@
+import { Connection } from 'mongoose';
 export * from './auth/token-strategy';
+
+export interface RabbitMQPlugin {
+  publish: <T>(channel: string, data: T) => void;
+  sendToQueue: <T>(channel: string, data: T) => void;
+}
+export interface MongoosePlugin {
+  connection: Connection;
+}
