dacdd6089b54fb821c1477247fbe7b6dbb249ed1
Jesús Jiménez <jesuswebdev@gmail.com>
2021-12-10 17:55:10 -0400

    add nats plugin interface

--- common/src/hapi/index.ts
+++ common/src/hapi/index.ts
@@ -1,9 +1,8 @@
 import { Connection } from 'mongoose';
 export * from './auth/token-strategy';
 
-export interface RabbitMQPlugin {
+export interface NATSPlugin {
   publish: <T>(channel: string, data: T) => void;
-  sendToQueue: <T>(channel: string, data: T) => void;
 }
 export interface MongoosePlugin {
   connection: Connection;
