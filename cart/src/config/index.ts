d25a945635853e9d69523f747b78d2e093645e12
Jesús Jiménez <jesuswebdev@gmail.com>
2021-12-12 15:40:52 -0400

    fix env variable names

--- cart/src/config/index.ts
+++ cart/src/config/index.ts
@@ -1,10 +1,10 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const HOST = process.env.AUTH_SERVICE_HOST ?? '0.0.0.0';
-export const PORT = process.env.AUTH_SERVICE_PORT ?? 8080;
+export const HOST = process.env.CART_SERVICE_HOST ?? '0.0.0.0';
+export const PORT = process.env.CART_SERVICE_PORT ?? 8080;
 export const MONGODB_URI =
-  `mongodb://${process.env.AUTH_MONGO_SERVICE_HOST}:${process.env.AUTH_MONGO_SERVICE_PORT}/delifood-cart` ??
+  `mongodb://${process.env.CART_MONGO_SERVICE_HOST}:${process.env.CART_MONGO_SERVICE_PORT}/delifood-cart` ??
   '';
 export const NATS_URI =
   `http://${process.env.NATS_SERVICE_HOST}:${process.env.NATS_SERVICE_PORT}` ??
--- products/src/config/index.ts
+++ products/src/config/index.ts
@@ -1,10 +1,10 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const HOST = process.env.AUTH_SERVICE_HOST ?? '0.0.0.0';
-export const PORT = process.env.AUTH_SERVICE_PORT ?? 8080;
+export const HOST = process.env.PRODUCTS_SERVICE_HOST ?? '0.0.0.0';
+export const PORT = process.env.PRODUCTS_SERVICE_PORT ?? 8080;
 export const MONGODB_URI =
-  `mongodb://${process.env.AUTH_MONGO_SERVICE_HOST}:${process.env.AUTH_MONGO_SERVICE_PORT}/delifood-products` ??
+  `mongodb://${process.env.PRODUCTS_MONGO_SERVICE_HOST}:${process.env.PRODUCTS_MONGO_SERVICE_PORT}/delifood-products` ??
   '';
 export const NATS_URI =
   `http://${process.env.NATS_SERVICE_HOST}:${process.env.NATS_SERVICE_PORT}` ??
